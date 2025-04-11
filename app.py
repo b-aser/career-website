51d098a6f497274c27fae5995a0bf3ac1bec5f57
unknown <asertesfaye@hotmail.com>
2025-04-11 05:28:21 +0300

    added dynaminc data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
-
+companyname = "Aser"
 JOBS = [
     {
         'id' : 1,
@@ -31,7 +31,12 @@ JOBS = [
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name= companyname)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
